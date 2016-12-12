e7eaba1d3626f389c5a8620cf5bef4bb81e99bd4
Olivier Chafik <olivier.chafik@gmail.com>
2016-12-12 01:45:35 +0000

    Add triangulation with code shared w/ moralization

--- TSPort/src/graph/index.ts
+++ TSPort/src/graph/index.ts
@@ -2,5 +2,5 @@ export {DirectedGraph} from './directed_graph';
 export {UndirectedGraph} from './undirected_graph';
 export {Edge} from './edge';
 export {buildJunctionGraph} from './junction';
-export {moralize} from './moralization';
+export {moralize, triangulate} from './moralization';
 export {Clique, growCliques} from './cliques';
--- TSPort/src/graph/moralization.ts
+++ TSPort/src/graph/moralization.ts
@@ -1,20 +1,31 @@
+import * as Immutable from 'immutable';
 import {DirectedGraph, UndirectedGraph, Edge} from '.';
 
-export function moralize<V>(graph: DirectedGraph<V, {}>, isLessThan: IsLessThan<V>): UndirectedGraph<V, {}> {
+function commonTriangulation<V>(vertices: Immutable.Set<V>, getNeighbours: (vertex: V) => Immutable.Set<V>, hasEdge: (a: V, b: V) => boolean): Edge<{}, V>[] {
   let newEdges: Edge<{}, V>[] = [];
 
-  for (let vertex of graph.vertices.toArray()) {
-    let parents = graph.getOrigins(vertex).toArray();
+  for (let vertex of vertices.toArray()) {
+    let parents = getNeighbours(vertex).toArray();
     // "Marry" parents in non-oriented way:
     for (let i = 0; i < parents.length; i++) {
       for (let j = i + 1; j < parents.length; j++) {
         let pi = parents[i];
         let pj = parents[j];
-        if (!graph.hasEdge(pi, pj)) {
+        if (!hasEdge(pi, pj)) {
           newEdges.push(new Edge({from: pi, to: pj}));
         }
       }
     }
   }
+  return newEdges;
+}
+
+export function moralize<V>(graph: DirectedGraph<V, {}>, isLessThan: IsLessThan<V>): UndirectedGraph<V, {}> {
+  let newEdges = commonTriangulation<V>(graph.vertices, v => graph.getOrigins(v), (a, b) => graph.hasEdge(a, b));
   return graph.add({edges: newEdges}).toUndirected(isLessThan);
+}
+
+export function triangulate<V>(graph: UndirectedGraph<V, {}>): UndirectedGraph<V, {}> {
+  let newEdges = commonTriangulation<V>(graph.vertices, v => graph.getNeighbours(v), (a, b) => graph.hasEdge(a, b));
+  return graph.add({edges: newEdges});
 }
